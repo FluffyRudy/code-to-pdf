8d616f72588992504dc733264aee8cc05b2e8114
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-09-06 16:43:46 +0545

    Add default random  filename automatically

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def handle_text_accept(e):
 
 def save_pdf(content: str, save_to: str):
     html = HTML(string=content)
-    filename = f"{get_random_hex(4)}.pdf"
     html.write_pdf(save_to, font_config=FontConfiguration())
     print(content, save_to)
 
 def main():
     text = easygui.textbox(msg="Insert HTML code", callback=handle_text_accept)
     if text:
-        file_open = easygui.filesavebox(msg="enter filename", title="filemanager", default="./")
+        filename = f"{get_random_hex(4)}.pdf"
+        file_open = easygui.filesavebox(msg="enter filename", title="filemanager", default=filename)
         pdf_converter_thread = threading.Thread(target=save_pdf, args=(text, file_open))
         pdf_converter_thread.start()
 
