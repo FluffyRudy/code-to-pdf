90e9257e699c9fb8950f7d6570330503cc55ff70
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-09-06 15:43:05 +0545

    add gui

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-from typing import Optional
+import easygui
+import threading
 from weasyprint import HTML, CSS
 from weasyprint.text.fonts import FontConfiguration
-from pathlib import Path
 from hexgen import get_random_hex
 
+def handle_text_accept(e):
+    e.stop()
 
-class Html2Pdf:
-    def __init__(self, html: str, css: Optional[str] = None):
-        html_path = Path(html)
-        css_path = Path(css) if css is not None else None
+def save_pdf(content: str, save_to: str):
+    html = HTML(string=content)
+    filename = f"{get_random_hex(4)}.pdf"
+    html.write_pdf(save_to, font_config=FontConfiguration())
+    print(content, save_to)
 
-        if not html_path.exists():
-            raise FileNotFoundError("html file doesnt exist")
-        if css is not None and not css_path.exists():
-            raise FileExistsError("Css file doesnt exist")
-
-        self.html_instance: HTML = HTML(filename=html)
-        if css is not None:
-            self.css_instance: CSS = CSS(filename=css)
-        else:
-            self.css_instance: CSS = CSS(string="")
-        self.font_config = FontConfiguration()
-
-    def make_pdf(self, path: str = "."):
-        save_path = Path(path).resolve()
-
-        if save_path.is_dir():
-            filename = str(get_random_hex(4)) + ".pdf"
-            save_path = save_path.joinpath(filename)
-
-        try:
-            self.html_instance.write_pdf(
-                save_path, font_config=self.font_config, stylesheets=[self.css_instance]
-            )
-        except Exception as error:
-            print(error)
-
+def main():
+    text = easygui.textbox(msg="Insert HTML code", callback=handle_text_accept)
+    file_open = easygui.filesavebox(msg="enter filename", title="filemanager", default="./")
+    pdf_converter_thread = threading.Thread(target=save_pdf, args=(text, file_open))
+    pdf_converter_thread.start()
 
 if __name__ == "__main__":
-    Html2Pdf("./sample.html", "./sample.css").make_pdf()
+    main()
